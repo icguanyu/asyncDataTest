60f1f29be5426ea7de2207b993609e2445442088
Nick <icguanyu@gmail.com>
2022-03-22 10:58:39 +0800

    test

--- pages/rent/_id.vue
+++ pages/rent/_id.vue
@@ -5,10 +5,11 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 import { Tenders } from "@/api/Tenders";
 export default {
   name: "rent-id",
-  async asyncData({ params, query, error }) {
+  async asyncData({ params, query }) {
     console.log("asyncData! GetRentDetail");
     let query_params = {
       TenderBaseDataId: params.id,
@@ -17,16 +18,8 @@ export default {
     };
     let data;
     await Tenders.GetRentDetail(query_params).then((res) => {
-      if (res.status == 200) {
-        data = {
-          detail: res.data,
-          propertyDetails: res.data.propertyDetails,
-          yearTenderNos: res.data.yearTenderNos,
-          selectedYearTender: res.data.yearTenderNos[0],
-        };
-      } else {
-        error({ statusCode: 404, message: "找不到此標租物件。" });
-      }
+      console.log(res);
+      data = res.data;
     });
     return { data };
   },
