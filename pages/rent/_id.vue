487181707032beddf7e505b6d8d9e5a517980c07
Nick <icguanyu@gmail.com>
2022-03-22 11:25:43 +0800

    test

--- pages/rent/_id.vue
+++ pages/rent/_id.vue
@@ -5,31 +5,25 @@
   </div>
 </template>
 <script>
-import axios from "axios";
 import { Tenders } from "@/api/Tenders";
 export default {
   name: "rent-id",
   async asyncData({ params, query }) {
-    console.log("asyncData! GetRentDetail");
     let query_params = {
       TenderBaseDataId: params.id,
       TenderBuildingMainIds: query.main,
       GroupNo: query.groupNo,
     };
+    console.log("asyncData! GetRentDetail", query_params);
     let data;
-    await axios
-      .post(
-        "https://dorts.pascation.com.tw/api/Member/GetRentDetail",
-        query_params
-      )
-      .then((res) => {
-        console.log(res);
-        data = res.data;
-      });
+    await Tenders.GetRentDetail(query_params).then((res) => {
+      console.log(res);
+      data = res.data;
+    });
     return { data };
   },
   mounted() {
-    console.log("mounted!");
+    console.log("mounted!!!!");
   },
 };
 </script>
